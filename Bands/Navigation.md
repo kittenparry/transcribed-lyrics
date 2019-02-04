4f3448afa16bf38c7d963c88d8fac8a8e229dcf2
Edvin Boul <kittenparry@users.noreply.github.com>
2019-02-04 16:14:53 +0300

    Generate Navigation.md using Python

--- Bands/Navigation.md
+++ Bands/Navigation.md
@@ -1,3 +1,26 @@
+# Navigation
+## Contents
+1. [Conquest - The Harvest](#conquest---the-harvest)
+2. [Far From Reality - Reminiscence](#far-from-reality---reminiscence)
+---
 ### Conquest - The Harvest
-1. [Primitive](Conquest/The%20Harvest/01.%20Primitive)
-2. [Red Horizon](Conquest/The%20Harvest/02.%20Red%20Horizon)
+1. [Primitive](Conquest/The%20Harvest/Primitive)
+2. [Red Horizon](Conquest/The%20Harvest/Red%20Horizon)
+3. [Touch of Danger](Conquest/The%20Harvest/Touch%20of%20Danger)
+4. [The Harvest](Conquest/The%20Harvest/The%20Harvest)
+5. [The Path](Conquest/The%20Harvest/The%20Path)
+6. [Wings of Death](Conquest/The%20Harvest/Wings%20of%20Death)
+7. [Vanishing](Conquest/The%20Harvest/Vanishing)
+8. [Rock the Rook Off](Conquest/The%20Harvest/Rock%20the%20Rook%20Off)
+9. [Drifting in the Dark](Conquest/The%20Harvest/Drifting%20in%20the%20Dark)
+10. [Armageddon](Conquest/The%20Harvest/Armageddon)
+11. [Night in the Cold World](Conquest/The%20Harvest/Night%20in%20the%20Cold%20World)
+### Far From Reality - Reminiscence
+1. [Falter](Far%20From%20Reality/Reminiscence/Falter)
+2. [Waiting](Far%20From%20Reality/Reminiscence/Waiting)
+3. [Reminiscence Part I Lost](Far%20From%20Reality/Reminiscence/Reminiscence%20Part%20I%20Lost)
+4. [Reminiscence Part II Regrets](Far%20From%20Reality/Reminiscence/Reminiscence%20Part%20II%20Regrets)
+5. [Reminiscence Part III Home](Far%20From%20Reality/Reminiscence/Reminiscence%20Part%20III%20Home)
+6. [Abyss](Far%20From%20Reality/Reminiscence/Abyss)
+7. [Agony](Far%20From%20Reality/Reminiscence/Agony)
+8. [Summer Sunrise Melancholy](Far%20From%20Reality/Reminiscence/Summer%20Sunrise%20Melancholy)
---
+++ Extras/navigation_generator.py
@@ -0,0 +1,58 @@
+import os
+
+path_bands = '../Bands'
+path_subs = []
+files = []
+
+#get band names
+n_bands = [f for f in os.listdir(path_bands) if not f == 'Navigation.md']
+n_albums = []
+#get album paths
+for band in n_bands:
+  path_sub = f'{path_bands}/{band}/'
+  #this assumes one album per band should be changed
+  album = ''.join(os.listdir(path_sub))
+  path_sub = path_sub + album
+  n_albums.append(album)
+  path_subs.append(path_sub)
+#get song names
+for i in range(len(path_subs)):
+  files.append([f for f in os.listdir(path_subs[i]) if not f == 'README.md'])
+#remove the .md ending for links
+for file in files:
+  for i in range(len(file)):
+    # file[i] = file[i].rstrip('.md')
+    file[i] = file[i].replace('.md', '')
+#non-numbered song names
+n_songs = [f for f in files]
+for file in n_songs:
+  for i in range(len(file)):
+    file[i] = file[i][4:]
+
+paths = []
+#this also assumes one album per band
+#get paths for md file
+for i in range(len(n_bands)):
+  paths.append([])
+  paths[i] = [f for f in files[i]]
+  for x in range(len(paths[i])):
+    paths[i][x] = f'{n_bands[i]}/{n_albums[i]}/{paths[i][x]}'
+    paths[i][x] = paths[i][x].replace(' ', '%20')
+
+temp = ''
+temp += '# Navigation\n'
+temp += '## Contents\n'
+for i in range(len(n_bands)):
+  p = f'{n_bands[i]} - {n_albums[i]}'
+  temp += f'{i+1}. [{p}](#{p.lower().replace(" ", "-")})\n'
+temp += '---\n'
+#this also assumes one album per band
+for i in range(len(n_bands)):
+  temp += f'### {n_bands[i]} - {n_albums[i]}\n'
+  x = 1
+  for song in n_songs[i]:
+    temp += f'{x}. [{song}]({paths[i][x-1]})\n'
+    x += 1
+
+with open('../Bands/Navigation.md', 'w') as f:
+  f.write(temp)
